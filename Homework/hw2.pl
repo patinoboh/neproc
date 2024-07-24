70d2975f6524b2be7aff83adfa4c7324c02115ce
patinoboh <patrik.brocek@gmail.com>
2024-07-24 13:28:34 +0200

    plz let me push

--- Homework/hw2.pl
+++ Homework/hw2.pl
@@ -14,6 +14,36 @@
 %
 % flat([a,[[],b,[]],[c,[d]]], R).
 % R = [a,b,c,d].
+
+ex1([]).
+ex2([[]]).
+ex3([a,b,c]).
+ex4([a,[[],b,[]],[c,[d]]]).
+ex0([1,2,3]).
+
+% consult("hw2.pl").
+
+flat(X, R) :- flat(X, [], R).
+
+flat([], A, A) :- !.
+flat([X|XS], A, R):-!,
+    flat(XS, A, B),
+    flat(X, B, R).
+flat(X, A, [X|A]).
+
+
+allEmpty([]).
+allEmpty([[]|R]) :- allEmpty(R).
+
+tp(M, []) :- allEmpty(M), !.
+tp(M, [C|R]) :- 
+    splitLists(M, C, CS),
+    tp(CS, R).
+
+splitLists([],[],[]).
+splitLists([[X|XS]|XSS], [X|R], [XS|RS]):-
+    splitLists(XSS, R, RS).
+
 %
 % Tento predikát měl být deterministický (speciálně otestujte, že po odmítnutí
 % neprodukuje duplikátní/nesprávné výsledky). Pokuste se o efektivní
@@ -34,7 +64,17 @@
 %
 % transp([[a],[b,c],[d]], R).
 % false.
-%
+
+mat0([]).
+mat1([[],[],[]]).
+mat2([[1,2,3],[4,5,6],[7,8,9]]).
+mat3([[1,2,3],[4,5,6]]).
+mat4([[1],[2],[3]]).
+
+transp([],[]).
+
+
+
 % c) (BONUSOVÁ ÚLOHA) Implementuje vkládání prvku pro AVL stromy.
 %
 % Použijte následující reprezentaci:
--- Homework/hw4.hs
+++ Homework/hw4.hs
@@ -1,3 +1,5 @@
+import Control.Arrow (ArrowChoice(right))
+import Data.ByteString (sort)
 -- 4. úloha
 --
 -- 1) Implementujte kódování a dekódování RLE (https://en.wikipedia.org/wiki/Run-length_encoding):
@@ -6,13 +8,20 @@
 -- [(1,'h'),(1,'e'),(2,'l'),(1,'o')]
 --
 rleEncode :: (Eq a) => [a] -> [(Int, a)]
-rleEncode = undefined
+rleEncode [] = []
+rleEncode (x:xs) = (number, x) : rleEncode cutted
+    where
+        number = length $ takeWhile (== x) (x:xs)
+        cutted = dropWhile (== x) xs
+
 
 -- >>> rleDecode [(1,'h'),(1,'e'),(2,'l'),(1,'o')]
 -- "hello"
 --
 rleDecode :: [(Int, a)] -> [a]
-rleDecode = undefined
+rleDecode [] = []
+rleDecode ((n, x):xs) = replicate n x ++ rleDecode xs
+
 
 -- 2) Definujte nekonečný seznam všech prvočísel. Pokuste se o efektivní řešení.
 -- Poté pomocí něj definujte funkci, která v daném rozsahu najde dvojici po sobě
@@ -22,16 +31,36 @@ rleDecode = undefined
 -- [2,3,5,7,11]
 --
 primes :: [Integer]
-primes = undefined
+primes = sieve [2..]
+
+sieve :: [Integer] -> [Integer]
+sieve [] = []
+sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
 
 -- >>> gap 1000
 -- (887, 907)
+
+
+gap' :: [Integer] -> (Integer, Integer)
+gap' []         = (0,0)
+gap' [_]        = (0,0)
+gap' (x1:x2:xs) = go (x1,x2) (x2:xs) where
+    go largest [] = largest
+    go largest [_] = largest
+    go (m1, m2) (y1:y2:ys)
+        | m2-m1 >= y2-y1 = go (m1,m2) (y2:ys)
+        | otherwise      = go (y1,y2) (y2:ys)
+
+gap :: Integer -> (Integer, Integer)
+gap n = gap' (takeWhile (< n) primes)
+
+
 --
 -- >>> rleEncode (map gap [3 .. 1000])
 -- [(2,(2,3)),(6,(3,5)),(18,(7,11)),(68,(23,29)),(30,(89,97)),(414,(113,127)),(366,(523,541)),(94,(887,907))]
 --
-gap :: Integer -> (Integer, Integer)
-gap = undefined
+
 
 -- Prvním argumentem je konec rozsahu, začátek bude vždy 2. Můžete předpokládat,
 -- že konec bude alespoň 3.
@@ -39,10 +68,25 @@ gap = undefined
 -- 3) Implementujte mergesort, který vyhazuje duplikáty.
 
 mergeWith :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
-mergeWith = undefined
+mergeWith _ [] ys = ys
+mergeWith _ xs [] = xs
+mergeWith comparator (x:xs) (y:ys) = case comparator x y of
+    LT -> x : mergeWith comparator xs (y:ys)
+    GT -> y : mergeWith comparator (x:xs) ys
+    EQ -> y : mergeWith comparator xs ys
 
-sortWith  :: (a -> a -> Ordering) -> [a] -> [a]
-sortWith = undefined
+
+
+
+sortWith :: (a -> a -> Ordering) -> [a] -> [a]
+sortWith _ [] = []
+sortWith _ [x] = [x]
+sortWith f xs = mergeWith f (sortWith f left) (sortWith f right)
+    where
+        (left, right) = splitAt (length xs `div` 2) xs
+
+
+
 
 -- Prvním argumentem je funkce, která provádí porovnávání.
 -- Ordering je datový typ, který obsahuje 3 konstanty: LT, EQ, GT
@@ -68,7 +112,9 @@ sortWith = undefined
 -- ["ab","ac","ad","bc","bd","cd"]
 --
 combinations :: Int -> [a] -> [[a]]
-combinations = undefined
+combinations 0 _ = [[]]
+combinations _ [] = []
+combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
 
 -- permutations x vygeneruje seznam všech permutací. Na pořadí permutací ve
 -- výsledném seznamu nezáleží.
@@ -76,8 +122,16 @@ combinations = undefined
 -- >>> permutations "abc"
 -- ["abc","bac","bca","acb","cab","cba"]
 --
-permutations :: [a] -> [[a]]
-permutations = undefined
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations xs = [x:ps | x <- xs, ps <- permutations (remove x xs)]
+    where
+        remove _ [] = []
+        remove z (y:ys)
+            | z == y = ys
+            | otherwise = y : remove z ys
+
+
 
 -- Pomocí těchto funkcí definujte "variace" (občas najdete v české literatuře,
 -- v angličtině pro to termín asi neexistuje): kombinace, kde záleží na pořadí
