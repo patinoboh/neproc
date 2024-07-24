899048bad4a2a37ec5ab58bfd498fa4de1c9456b
patinoboh <patrik.brocek@gmail.com>
2024-07-24 13:22:27 +0200

    Interface/library to work with mazes

--- zapoctak/MazeUtils.hs
+++ zapoctak/MazeUtils.hs
@@ -1,7 +1,7 @@
 module MazeUtils (
     Maze,Cell(..),Point,printMaze,saveMaze,initializeMaze,setCell,
-    inBounds,movements,applyMove,hasCycle,
-    isValidMove,directions, generatePathPoints,setFree,setFree2
+    hasCycle,printMazeTrue,
+    generatePathPoints,setFree,setFree2
 ) where
 
 
@@ -22,37 +22,56 @@ instance Show Cell where
 
 type Point = (Int, Int)
 
-printMaze :: Maze -> IO ()
-printMaze = mapM_ (putStrLn . concatMap show)
+-- | Print maze to stdout
+-- Prints it without postprocessing - for manipulation I work with bigger maze that must cropped afterwards. This one does not crop it.
+printMazeTrue :: Maze -> IO ()
+printMazeTrue = mapM_ (putStrLn . concatMap show)
 
+
+-- | Print maze to stdout
+-- Prints it with postprocessing - crops the maze
+printMaze :: Maze -> IO ()
+printMaze maze = mapM_ (putStrLn . concatMap' show) croppedRows
+  where
+    croppedRows = tail $ init maze
+    concatMap' f = concatMap f . init . tail
+
+-- | Convert maze to string
+-- Converts maze without postprocessing - therefore I do not use it
 mazeToString :: Maze -> String
 mazeToString maze = intercalate "\n" $ map (concatMap show) maze
 
+-- | Save maze to file
+-- Saves maze without postprocessing - therefore I do not use it
 saveMaze :: FilePath -> Maze -> IO ()
 saveMaze path maze = writeFile path $ mazeToString maze
 
+-- | Initialize maze with walls
 initializeMaze :: Int -> Int -> Maze
 initializeMaze height width = replicate height (replicate width Wall)
 
+-- | Get height of the maze
 height :: Maze -> Int
 height = length
 
+-- | Get width of the maze
 width :: Maze -> Int
 width = length . head
 
 
-
+-- | Set cell in maze
 setCell :: Maze -> Point -> Cell -> Maze
 setCell maze (x, y) cell
-    | x < 0 || x >= length maze = maze  -- Check row index bounds
-    | y < 0 || y >= length (head maze) = maze  -- Check column index bounds
+    | x < 0 || x >= length maze = maze
+    | y < 0 || y >= length (head maze) = maze
     | otherwise = before ++ [newRow] ++ after
   where
     (before, row:after) = splitAt x maze
     (rowBefore, _:rowAfter) = splitAt y row
     newRow = rowBefore ++ (cell : rowAfter)
 
-
+-- | From list of crossroads generates all points on the path
+-- The list must be a valid continuos path
 generatePathPoints :: [Point] -> [Point]
 generatePathPoints [] = []
 generatePathPoints [p] = [p]
@@ -65,32 +84,17 @@ generatePathPoints (p1@(x1, y1):p2@(x2, y2):rest)
         | a <= b    = [a..b-1]
         | otherwise = [b+1..a]
 
+-- | For a list of crossroads sets all cells to Free
 setFree :: [Point] -> Maze -> Maze
 setFree crossroads maze = foldr (\point accMaze -> setCell accMaze point Free) maze points
     where 
         points = generatePathPoints crossroads
 
+-- | Sets only points in the list to free
 setFree2 :: [Point] -> Maze -> Maze
 setFree2 crossroads maze = foldr (\point accMaze -> setCell accMaze point Free) maze crossroads
 
-inBounds :: Int -> Int -> Point -> Bool
-inBounds height width (x, y) = x >= 0 && x < height && y >= 0 && y < width
-
-movements :: [Point]
-movements = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-directions :: [Point]
-directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-applyMove :: Point -> Point -> Point
-applyMove (x, y) (dx, dy) = (x + dx, y + dy)
-
-isValidMove :: Maze -> [Point] -> Point -> Bool
-isValidMove maze path (x, y) =
-    inBounds (length maze) (length (head maze)) (x, y) &&
-    notElem (x, y) path &&
-    (maze !! x !! y == Wall)
-
+-- | Check if the path (given as list of crossroads) has a cycle
 hasCycle :: [Point] -> Bool
 hasCycle points = length path /= length (nub path)
     where path = generatePathPoints points
